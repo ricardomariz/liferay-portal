28686dd844d06d24f27ce35b012dc921a6d2cb4f
Daniel Raposo <daniel.raposo@liferay.com>
2025-02-07 14:40:48 +0100

    LPD-48305 buildREST: Auto generate API client for change-tracking-rest with new TypeScriptClientGenerator

--- modules/apps/change-tracking/change-tracking-rest-client-js/src/node/api/apis.ts
+++ modules/apps/change-tracking/change-tracking-rest-client-js/src/node/api/apis.ts
@@ -1,3 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 export * from './cTCollectionApi';
 import { CTCollectionApi } from './cTCollectionApi';
 export * from './cTEntryApi';
@@ -6,15 +11,13 @@ export * from './cTProcessApi';
 import { CTProcessApi } from './cTProcessApi';
 export * from './cTRemoteApi';
 import { CTRemoteApi } from './cTRemoteApi';
+
 import * as http from 'http';
-
 export class HttpError extends Error {
-    constructor (public response: http.IncomingMessage, public body: any, public statusCode?: number) {
-        super('HTTP request failed');
-        this.name = 'HttpError';
-    }
+	constructor (public response: http.IncomingMessage, public body: any, public statusCode?: number) {
+		super('HTTP request failed');
+		this.name = 'HttpError';
+	}
 }
-
-export { RequestFile } from '../model/models';
 
 export const APIS = [CTCollectionApi, CTEntryApi, CTProcessApi, CTRemoteApi];
