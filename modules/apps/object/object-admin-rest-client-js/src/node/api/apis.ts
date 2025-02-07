c985ac2c8f4b37ecf3ef8dba2c7a99fc863a839d
Daniel Raposo <daniel.raposo@liferay.com>
2025-02-07 13:21:39 +0100

    LPD-48305 buildREST: Auto generate API client for object-admin-rest with new TypeScriptClientGenerator

--- modules/apps/object/object-admin-rest-client-js/src/node/api/apis.ts
+++ modules/apps/object/object-admin-rest-client-js/src/node/api/apis.ts
@@ -1,3 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
 export * from './objectActionApi';
 import { ObjectActionApi } from './objectActionApi';
 export * from './objectDefinitionApi';
@@ -14,15 +19,13 @@ export * from './objectValidationRuleApi';
 import { ObjectValidationRuleApi } from './objectValidationRuleApi';
 export * from './objectViewApi';
 import { ObjectViewApi } from './objectViewApi';
+
 import * as http from 'http';
-
 export class HttpError extends Error {
-    constructor (public response: http.IncomingMessage, public body: any, public statusCode?: number) {
-        super('HTTP request failed');
-        this.name = 'HttpError';
-    }
+	constructor (public response: http.IncomingMessage, public body: any, public statusCode?: number) {
+		super('HTTP request failed');
+		this.name = 'HttpError';
+	}
 }
-
-export { RequestFile } from '../model/models';
 
 export const APIS = [ObjectActionApi, ObjectDefinitionApi, ObjectFieldApi, ObjectFolderApi, ObjectLayoutApi, ObjectRelationshipApi, ObjectValidationRuleApi, ObjectViewApi];
