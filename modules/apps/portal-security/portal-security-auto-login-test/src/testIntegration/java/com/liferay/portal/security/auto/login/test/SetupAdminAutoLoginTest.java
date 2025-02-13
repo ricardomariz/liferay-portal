c8dbef55c5761dae7cec3a2de91abfa31a214a07
Manuele Castro <manuele.castro@liferay.com>
2025-02-13 19:08:14 -0300

    LPD-41047 Fix com.liferay.portal.security.auto.l***n.test.SetupAdminAutoL***nTest

--- modules/apps/portal-security/portal-security-auto-login-test/src/testIntegration/java/com/liferay/portal/security/auto/login/test/SetupAdminAutoLoginTest.java
+++ modules/apps/portal-security/portal-security-auto-login-test/src/testIntegration/java/com/liferay/portal/security/auto/login/test/SetupAdminAutoLoginTest.java
@@ -15,6 +15,7 @@ import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.security.auto.login.AutoLogin;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -29,6 +30,7 @@ import com.liferay.portal.kernel.test.util.PropsValuesTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.struts.Action;
@@ -67,6 +69,10 @@ public class SetupAdminAutoLoginTest {
 
 		_company = CompanyTestUtil.addCompany();
 
+		_originalCompanyId = CompanyThreadLocal.getCompanyId();
+
+		CompanyThreadLocal.setCompanyId(_company.getCompanyId());
+
 		try {
 			_emailAdressAdminUser =
 				PropsValues.DEFAULT_ADMIN_EMAIL_ADDRESS_PREFIX + StringPool.AT +
@@ -85,6 +91,8 @@ public class SetupAdminAutoLoginTest {
 	@AfterClass
 	public static void tearDownClass() {
 		_safeCloseable.close();
+
+		CompanyThreadLocal.setCompanyId(_originalCompanyId);
 	}
 
 	@Test
@@ -137,12 +145,16 @@ public class SetupAdminAutoLoginTest {
 
 		mockHttpServletRequest.addParameter("p_auth", "test");
 
+		mockHttpServletRequest.setAttribute(
+			WebKeys.COMPANY_ID, _company.getCompanyId());
+
 		HttpSession httpSession = mockHttpServletRequest.getSession();
 
 		httpSession.setAttribute(
 			"LIFERAY_SHARED_AUTHENTICATION_TOKEN#CSRF", "test");
 
-		Layout layout = LayoutLocalServiceUtil.getLayout(1);
+		Layout layout = LayoutLocalServiceUtil.getLayout(
+			PortalUtil.getControlPanelPlid(_company.getCompanyId()));
 
 		ThemeDisplay themeDisplay = new ThemeDisplay();
 
@@ -172,6 +184,7 @@ public class SetupAdminAutoLoginTest {
 
 	private static Company _company;
 	private static String _emailAdressAdminUser;
+	private static long _originalCompanyId;
 	private static SafeCloseable _safeCloseable;
 	private static User _user;
 
