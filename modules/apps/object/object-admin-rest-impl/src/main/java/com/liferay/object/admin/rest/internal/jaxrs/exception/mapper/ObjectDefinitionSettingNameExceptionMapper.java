9f91fb4af525460a87aac36d7d4c75c25d8d5f7c
Pedro Tavares <pedro.tavares@liferay.com>
2025-02-14 13:45:14 -0300

    LPD-48902 Create exception mappers

---
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/jaxrs/exception/mapper/ObjectDefinitionSettingNameExceptionMapper.java
@@ -0,0 +1,38 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.admin.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.object.exception.ObjectDefinitionSettingNameException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pedro Tavares
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Object.Admin.REST)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Object.Admin.REST.ObjectDefinitionSettingNameExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class ObjectDefinitionSettingNameExceptionMapper
+	extends BaseExceptionMapper<ObjectDefinitionSettingNameException> {
+
+	@Override
+	protected Problem getProblem(
+		ObjectDefinitionSettingNameException
+			objectDefinitionSettingNameException) {
+
+		return new Problem(objectDefinitionSettingNameException);
+	}
+
+}
