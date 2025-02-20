3577aba4cae56d77f5996a783dcad306aa7966c4
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:00 +0100

    LPD-47925 @liferay-commerce - modules/apps/commerce/commerce-payment-web

--- modules/apps/commerce/commerce-payment-web/src/main/resources/META-INF/resources/js/DefaultCommercePaymentMethod.js
+++ modules/apps/commerce/commerce-payment-web/src/main/resources/META-INF/resources/js/DefaultCommercePaymentMethod.js
@@ -3,11 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {
-	createPortletURL,
-	delegate,
-	openSelectionModal as openSelectionModalUtil,
-} from 'frontend-js-web';
+import {openSelectionModal as openSelectionModalUtil} from 'frontend-js-components-web';
+import {createPortletURL, delegate} from 'frontend-js-web';
 
 export default function ({
 	baseSelectDefaultCommercePaymentMethodURL,
