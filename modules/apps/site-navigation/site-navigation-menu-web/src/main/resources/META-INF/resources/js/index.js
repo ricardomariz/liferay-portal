363a44fd8873f03624d28924479d3275d9cfe001
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:48 +0100

    LPD-47925 @liferay-site-management - modules/apps/site-navigation/site-navigation-menu-web

--- modules/apps/site-navigation/site-navigation-menu-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/site-navigation/site-navigation-menu-web/src/main/resources/META-INF/resources/js/index.js
@@ -3,12 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {openSelectionModal} from 'frontend-js-components-web';
 import {
 	addParams,
 	debounce,
 	delegate,
 	getFormElement,
-	openSelectionModal,
 	toggleDisabled,
 	toggleSelectBox,
 } from 'frontend-js-web';
