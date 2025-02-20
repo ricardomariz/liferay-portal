4e4c5baa253d3a7c9aa88f651a8de88e077d2047
Mikel Lorza <mikel.lorza@liferay.com>
2025-02-20 12:25:16 +0100

    LPD-47351 Include ObjectEntryFolder in content section instead of DLFolder

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/configuration/CMSSiteInitializerConfiguration.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/configuration/CMSSiteInitializerConfiguration.java
@@ -27,7 +27,7 @@ public interface CMSSiteInitializerConfiguration {
 	public String[] categorizationClassNames();
 
 	@Meta.AD(
-		deflt = "com.liferay.blogs.model.BlogsEntry|com.liferay.bookmarks.model.BookmarksEntry|com.liferay.bookmarks.model.BookmarksFolder|com.liferay.document.library.kernel.model.DLFileShortcut|com.liferay.document.library.kernel.model.DLFolder|com.liferay.dynamic.data.mapping.model.DDMFormInstance|com.liferay.journal.model.JournalArticle|com.liferay.journal.model.JournalFolder|com.liferay.knowledge.base.model.KBArticle|com.liferay.knowledge.base.model.KBFolder|com.liferay.message.boards.model.MBCategory|com.liferay.message.boards.model.MBThread",
+		deflt = "com.liferay.blogs.model.BlogsEntry|com.liferay.bookmarks.model.BookmarksEntry|com.liferay.bookmarks.model.BookmarksFolder|com.liferay.document.library.kernel.model.DLFileShortcut|com.liferay.document.library.kernel.model.DLFolder|com.liferay.dynamic.data.mapping.model.DDMFormInstance|com.liferay.journal.model.JournalArticle|com.liferay.journal.model.JournalFolder|com.liferay.knowledge.base.model.KBArticle|com.liferay.knowledge.base.model.KBFolder|com.liferay.message.boards.model.MBCategory|com.liferay.message.boards.model.MBThread|com.liferay.object.model.ObjectEntryFolder",
 		name = "contents-class-names", required = false
 	)
 	public String[] contentsClassNames();
