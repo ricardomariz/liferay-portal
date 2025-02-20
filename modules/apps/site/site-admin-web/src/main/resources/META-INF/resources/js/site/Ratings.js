dbf672732cece7a284531a8e1e2af5627e38c332
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:49 +0100

    LPD-47925 @liferay-site-management - modules/apps/site/site-admin-web

--- modules/apps/site/site-admin-web/src/main/resources/META-INF/resources/js/site/Ratings.js
+++ modules/apps/site/site-admin-web/src/main/resources/META-INF/resources/js/site/Ratings.js
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {delegate, openConfirmModal} from 'frontend-js-web';
+import {openConfirmModal} from 'frontend-js-components-web';
+import {delegate} from 'frontend-js-web';
 
 export default function ({namespace}) {
 	const ratingSettingsContainer = document.getElementById(
