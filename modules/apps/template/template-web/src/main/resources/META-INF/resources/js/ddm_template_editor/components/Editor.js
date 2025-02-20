7b796a442f84eccc58c9700df19ee8003a09b72d
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:39 +0100

    LPD-47925 @liferay-page-management - modules/apps/template/template-web

--- modules/apps/template/template-web/src/main/resources/META-INF/resources/js/ddm_template_editor/components/Editor.js
+++ modules/apps/template/template-web/src/main/resources/META-INF/resources/js/ddm_template_editor/components/Editor.js
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {fetch, navigate, openToast, sub} from 'frontend-js-web';
+import {openToast} from 'frontend-js-components-web';
+import {fetch, navigate, sub} from 'frontend-js-web';
 import PropTypes from 'prop-types';
 import React, {useContext, useEffect, useRef, useState} from 'react';
 
