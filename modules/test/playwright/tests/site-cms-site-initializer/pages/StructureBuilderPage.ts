45ddc36282d1519c1b902679f98b37480fc34965
Sandro Chinea <sandrochinea@gmail.com>
2025-02-20 13:38:33 +0000

    LPD-48502 Add playwright test

--- modules/test/playwright/tests/site-cms-site-initializer/pages/StructureBuilderPage.ts
+++ modules/test/playwright/tests/site-cms-site-initializer/pages/StructureBuilderPage.ts
@@ -3,18 +3,73 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Page} from '@playwright/test';
+import {Locator, Page} from '@playwright/test';
 
+import {clickAndExpectToBeVisible} from '../../../utils/clickAndExpectToBeVisible';
 import {PORTLET_URLS} from '../../../utils/portletUrls';
+import {waitForAlert} from '../../../utils/waitForAlert';
+
+type FieldType = 'Text';
 
 export class StructureBuilderPage {
 	readonly page: Page;
 
+	private readonly labelInput: Locator;
+	private readonly publishButton: Locator;
+	private readonly saveButton: Locator;
+
 	constructor(page: Page) {
 		this.page = page;
+
+		this.labelInput = this.page.getByLabel('Structure Label');
+		this.publishButton = this.page.getByRole('button', {name: 'Publish'});
+		this.saveButton = this.page.getByRole('button', {name: 'Save'});
 	}
 
 	async goto() {
 		await this.page.goto(PORTLET_URLS.cmsStructureBuilder);
+
+		await this.page.getByText('New Structure').waitFor();
+	}
+
+	async addField(type: FieldType) {
+		const hasFields = !(await this.page
+			.getByText('No Fields Yet')
+			.isVisible());
+
+		let trigger: Locator;
+
+		if (hasFields) {
+			trigger = this.page.getByLabel('Add Field');
+		}
+		else {
+			trigger = this.page.getByText('Add Field');
+		}
+
+		await clickAndExpectToBeVisible({
+			autoClick: true,
+			target: this.page.getByRole('menuitem', {name: type}),
+			trigger,
+		});
+	}
+
+	async changeStructureLabel(label: string) {
+		await this.labelInput.fill(label);
+
+		await this.page.locator('span.label-item').click();
+	}
+
+	async publishStructure() {
+		await this.publishButton.click();
+
+		await waitForAlert(this.page, 'published successfully', {
+			timeout: 1000,
+		});
+	}
+
+	async saveStructure() {
+		await this.saveButton.click();
+
+		await waitForAlert(this.page, 'successfully', {timeout: 1000});
 	}
 }
---
+++ modules/test/playwright/tests/site-cms-site-initializer/structureBuilder.spec.ts
@@ -0,0 +1,88 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {ObjectDefinitionApi} from '@liferay/object-admin-rest-client-js';
+import {expect, mergeTests} from '@playwright/test';
+
+import {apiHelpersTest} from '../../fixtures/apiHelpersTest';
+import {featureFlagsTest} from '../../fixtures/featureFlagsTest';
+import {loginTest} from '../../fixtures/loginTest';
+import {getRandomInt} from '../../utils/getRandomInt';
+import {cmsPagesTest} from './fixtures/cmsPagesTest';
+
+const test = mergeTests(
+	apiHelpersTest,
+	cmsPagesTest,
+	featureFlagsTest({
+		'LPD-11232': {enabled: true},
+		'LPD-17809': {enabled: true},
+	}),
+	loginTest()
+);
+
+test('Structures can be saved and published', async ({
+	apiHelpers,
+	page,
+	structureBuilderPage,
+}) => {
+
+	// Go to the Structure Builder
+
+	await structureBuilderPage.goto();
+
+	// Check invalid labels don't work
+
+	await structureBuilderPage.changeStructureLabel('1');
+
+	await expect(async () => {
+		await structureBuilderPage.saveStructure();
+	}).not.toPass();
+
+	// Put valid label
+
+	const label = `Structure ${getRandomInt()}`;
+
+	await structureBuilderPage.changeStructureLabel(label);
+
+	// Save structure and capture object definition id
+
+	const [response] = await Promise.all([
+		page.waitForResponse(
+			(response) =>
+				response.url().includes('object-definitions') &&
+				response.status() === 200
+		),
+		await structureBuilderPage.saveStructure(),
+	]);
+
+	const {id} = await response.json();
+
+	await expect(page.locator('.alert-danger')).not.toBeVisible();
+
+	// Check we can't publish without adding a field
+
+	await expect(async () => {
+		await structureBuilderPage.publishStructure();
+	}).not.toPass();
+
+	// Add a field
+
+	await structureBuilderPage.addField('Text');
+
+	// Save it again and publish it
+
+	await structureBuilderPage.saveStructure();
+	await structureBuilderPage.publishStructure();
+
+	// Delete structure
+
+	const APIClient = await apiHelpers.buildRestClient(ObjectDefinitionApi);
+
+	const {
+		response: {statusCode},
+	} = await APIClient.deleteObjectDefinition(id);
+
+	expect(statusCode).toBe(204);
+});
