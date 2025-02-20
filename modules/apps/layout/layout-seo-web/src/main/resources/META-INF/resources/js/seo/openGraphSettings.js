3bfe76aab8f8caad669236dd696e1693e9d5484e
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:37 +0100

    LPD-47925 @liferay-page-management - modules/apps/layout/layout-seo-web

--- modules/apps/layout/layout-seo-web/src/main/resources/META-INF/resources/js/seo/openGraphSettings.js
+++ modules/apps/layout/layout-seo-web/src/main/resources/META-INF/resources/js/seo/openGraphSettings.js
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {openSelectionModal, toggleDisabled} from 'frontend-js-web';
+import {openSelectionModal} from 'frontend-js-components-web';
+import {toggleDisabled} from 'frontend-js-web';
 
 export default function ({namespace, uploadOpenGraphImageURL}) {
 	const openGraphImageButton = document.getElementById(
