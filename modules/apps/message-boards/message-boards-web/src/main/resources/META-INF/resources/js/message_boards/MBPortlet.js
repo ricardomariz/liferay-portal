845a2f22ea04ac691c82d76e1254f0658dc1f01d
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:19 +0100

    LPD-47925 @liferay-content-management - modules/apps/message-boards/message-boards-web

--- modules/apps/message-boards/message-boards-web/src/main/resources/META-INF/resources/js/message_boards/MBPortlet.js
+++ modules/apps/message-boards/message-boards-web/src/main/resources/META-INF/resources/js/message_boards/MBPortlet.js
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {fetch, openConfirmModal, openModal, sub} from 'frontend-js-web';
+import {openConfirmModal, openModal} from 'frontend-js-components-web';
+import {fetch, sub} from 'frontend-js-web';
 
 const RECENTLY_REMOVED_ATTACHMENTS = {
 	multiple: Liferay.Language.get('x-recently-removed-attachments'),
--- modules/apps/message-boards/message-boards-web/src/main/resources/META-INF/resources/message_boards_admin/js/MBEntriesManagementToolbarPropsTransformer.js
+++ modules/apps/message-boards/message-boards-web/src/main/resources/META-INF/resources/message_boards_admin/js/MBEntriesManagementToolbarPropsTransformer.js
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {openConfirmModal, postForm} from 'frontend-js-web';
+import {openConfirmModal} from 'frontend-js-components-web';
+import {postForm} from 'frontend-js-web';
 
 export default function propsTransformer({
 	additionalProps: {
