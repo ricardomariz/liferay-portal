4ece3879e4c434d202c0fda423bf9196b90405e7
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:17 +0100

    LPD-47925 @liferay-content-management - modules/apps/item-selector/item-selector-web

--- modules/apps/item-selector/item-selector-web/src/main/resources/META-INF/resources/js/openItemSelectorModal.ts
+++ modules/apps/item-selector/item-selector-web/src/main/resources/META-INF/resources/js/openItemSelectorModal.ts
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {createPortletURL, openSelectionModal} from 'frontend-js-web';
+import {openSelectionModal} from 'frontend-js-components-web';
+import {createPortletURL} from 'frontend-js-web';
 
 const openItemSelectorModal = ({multiple, params, url, ...props}: any) => {
 	openSelectionModal({
