4e76e589a15a64cb25f0c443b8de3f0b9bc612c5
Petteri Karttunen <petteri.karttunen@liferay.com>
2025-02-21 01:57:42 +0200

    LPD-49048 export-import-web: Add Playwright test

--- modules/test/playwright/tests/export-import-web/instance.export.spec.ts
+++ modules/test/playwright/tests/export-import-web/instance.export.spec.ts
@@ -19,6 +19,7 @@ import performLogin, {performLogout, userData} from '../../utils/performLogin';
 import {getTempDir} from '../../utils/temp';
 import {readFileFromZip} from '../../utils/zip';
 import {companyExportImportPageTest} from './fixtures/companyExportImportPagesTest';
+import {toDateRangeDate, toDateRangeTime} from './utils/dateRangeUtil';
 
 export const test = mergeTests(
 	applicationsMenuPageTest,
@@ -84,6 +85,102 @@ test('cannot export site scoped custom object entries at instance level', async 
 	await expect(page.getByLabel('Tests 1 Items')).toBeHidden();
 });
 
+test('can export custom object entries at instance level with date filter', async ({
+	apiHelpers,
+	companyExportImportPage,
+}) => {
+	const objectActionApiClient =
+		await apiHelpers.buildRestClient(ObjectDefinitionApi);
+
+	const {body: objectDefinition} =
+		await objectActionApiClient.postObjectDefinition({
+			active: true,
+			externalReferenceCode: 'test',
+			label: {
+				en_US: 'Test',
+			},
+			name: 'Test',
+			objectFields: [
+				{
+					DBType: ObjectField.DBTypeEnum.String,
+					businessType: ObjectField.BusinessTypeEnum.Text,
+					indexed: true,
+					indexedAsKeyword: true,
+					label: {
+						en_US: 'Name',
+					},
+					name: 'name',
+					required: true,
+				},
+			],
+			pluralLabel: {
+				en_US: 'Tests',
+			},
+			portlet: true,
+			scope: 'company',
+			status: {
+				code: 0,
+			},
+		});
+
+	apiHelpers.data.push({id: objectDefinition.id, type: 'objectDefinition'});
+
+	await apiHelpers.objectEntry.postObjectEntry(
+		{externalReferenceCode: '', name: 'test'},
+		'c/tests'
+	);
+
+	const exportFilePath1 = await companyExportImportPage.export(
+		'Tests 1 Items',
+		false
+	);
+
+	const content1 = await readFileFromZip('C_Test.json', exportFilePath1);
+
+	const json1 = JSON.parse(content1);
+
+	expect(json1.length).toBe(1);
+
+	const endDate = new Date();
+
+	endDate.setDate(endDate.getDate() - 1);
+
+	const startDate = new Date();
+
+	startDate.setDate(startDate.getDate() - 2);
+
+	const exportFilePath2 = await companyExportImportPage.export(
+		'Tests 1 Items',
+		false,
+		{
+			endDate: toDateRangeDate(endDate),
+			endTime: toDateRangeTime(endDate),
+			startDate: toDateRangeDate(startDate),
+			startTime: toDateRangeTime(startDate),
+		}
+	);
+
+	const content2 = await readFileFromZip('C_Test.json', exportFilePath2);
+
+	const json2 = JSON.parse(content2);
+
+	expect(json2.length).toBe(0);
+
+	const exportFilePath3 = await companyExportImportPage.export(
+		'Tests 1 Items',
+		false,
+		{
+			rangeLast: '12 Hours',
+		}
+	);
+
+	const content3 = await readFileFromZip('C_Test.json', exportFilePath3);
+
+	const json3 = JSON.parse(content3);
+
+	expect(json3.length).toBe(1);
+});
+
 test('can export new default and custom task name', async ({
 	apiHelpers,
 	companyExportImportPage,
@@ -136,6 +233,7 @@ test('can export new default and custom task name', async ({
 	const customExportFilePath = await companyExportImportPage.export(
 		'Tests 1 Items',
 		false,
+		undefined,
 		taskName
 	);
 
