2d3195630d4cf96e5865e1607deb048d70dba70c
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:51 +0100

    LPD-47925 @liferay-site-management - modules/dxp/apps/osb/osb-site-initializer

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-liferay-online/src/main/resources/site-initializer/layout-set/private/js.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-liferay-online/src/main/resources/site-initializer/layout-set/private/js.js
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {fetch, getOpener, openModal} from 'frontend-js-web';
+import {openModal} from 'frontend-js-components-web';
+import {fetch, getOpener} from 'frontend-js-web';
 
 let copySaved = '';
 
