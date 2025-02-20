99178e2116bc71953d4a0a3509f13c50f83a12ec
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:14 +0100

    LPD-47925 @liferay-content-management - modules/apps/comment/comment-taglib

--- modules/apps/comment/comment-taglib/src/main/resources/META-INF/resources/discussion/js/Comments.js
+++ modules/apps/comment/comment-taglib/src/main/resources/META-INF/resources/discussion/js/Comments.js
@@ -3,11 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import {openToast} from 'frontend-js-components-web';
 import {
 	fetch,
 	getFormElement,
 	objectToFormData,
-	openToast,
 	openWindow,
 	runScriptsInElement,
 	setFormValues,
