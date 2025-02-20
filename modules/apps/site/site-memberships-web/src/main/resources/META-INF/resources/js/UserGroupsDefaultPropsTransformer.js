9d01cf9a385254ffa6b69906db6e2696b6d0cf58
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-02-20 10:03:49 +0100

    LPD-47925 @liferay-site-management - modules/apps/site/site-memberships-web

--- modules/apps/site/site-memberships-web/src/main/resources/META-INF/resources/js/UserGroupsDefaultPropsTransformer.js
+++ modules/apps/site/site-memberships-web/src/main/resources/META-INF/resources/js/UserGroupsDefaultPropsTransformer.js
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {openSelectionModal, setFormValues} from 'frontend-js-web';
+import {openSelectionModal} from 'frontend-js-components-web';
+import {setFormValues} from 'frontend-js-web';
 
 const ACTIONS = {
 	assignUserGroupRole(itemData, portletNamespace) {
