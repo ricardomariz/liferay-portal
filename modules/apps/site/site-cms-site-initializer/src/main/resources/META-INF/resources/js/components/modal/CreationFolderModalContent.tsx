0c30d62c67ae0b71c0e51e8e1dcb6832882394cc
Jorge González <jorge.gonzalez@liferay.com>
2025-02-20 12:51:11 +0100

    LPD-47337 Add fetch asset libraries on open and manage loading

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/components/modal/CreationFolderModalContent.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/components/modal/CreationFolderModalContent.tsx
@@ -4,28 +4,64 @@
  */
 
 import ClayButton from '@clayui/button';
+import {Option, Picker} from '@clayui/core';
 import Form, {ClayInput} from '@clayui/form';
+import ClayLoadingIndicator from '@clayui/loading-indicator';
 import ClayModal from '@clayui/modal';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import {getAssetsLibrariesByCompany} from '../../api/api';
 
 export default function CreationFolderModalContent({
 	closeModal,
 }: {
 	closeModal: voidReturn;
 }) {
+	const [assetLibraries, setAssetsLibraries] = useState<
+		{id: number; name: string}[]
+	>([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		getAssetsLibrariesByCompany().then((result: any) => {
+			setAssetsLibraries(result);
+			setLoading(false);
+		});
+	}, []);
+
 	return (
 		<>
 			<ClayModal.Header>
 				{Liferay.Language.get('new-folder')}
 			</ClayModal.Header>
 			<ClayModal.Body>
-				<Form.Group>
-					<label htmlFor="folferName">
-						{Liferay.Language.get('name')}
-					</label>
+				{loading ? (
+					<div className="loader-container">
+						<ClayLoadingIndicator />
+					</div>
+				) : (
+					<>
+						<Form.Group>
+							<label htmlFor="folderName">
+								{Liferay.Language.get('name')}
+							</label>
 
-					<ClayInput id="folferName" type="text" />
-				</Form.Group>
+							<ClayInput id="folderName" type="text" />
+						</Form.Group>
+
+						<Form.Group>
+							<label htmlFor="space">
+								{Liferay.Language.get('space')}
+							</label>
+
+							<Picker id="space" items={assetLibraries}>
+								{({id, name}) => (
+									<Option key={id}>{name}</Option>
+								)}
+							</Picker>
+						</Form.Group>
+					</>
+				)}
 			</ClayModal.Body>
 			<ClayModal.Footer
 				last={
