0f6ca1939afd0f31f3976c89300eb1cfb04e574a
Alan Huang <alan.huang@liferay.com>
2025-02-21 13:40:36 +0800

    LPD-49592 JAAS should be capitalized

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/CapsNameCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/CapsNameCheck.java
@@ -71,7 +71,8 @@ public class CapsNameCheck extends BaseCheck {
 	}
 
 	private static final String[][] _ALL_CAPS_STRINGS = {
-		{"DDL", "Ddl"}, {"DDM", "Ddm"}, {"DL", "Dl"}, {"PK", "Pk"}
+		{"DDL", "Ddl"}, {"DDM", "Ddm"}, {"DL", "Dl"}, {"JAAS", "Jaas"},
+		{"PK", "Pk"}
 	};
 
 	private static final String _MSG_RENAME = "rename";
