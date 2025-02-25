9e7ce852567e7de55e3b6be7faebe345d3e91a64
Manuele Castro <manuele.castro@liferay.com>
2025-02-25 19:58:16 -0300

    LPD-49757 Add CSP page

---
+++ modules/test/playwright/pages/portal-security-content-security-policy/ContentSecurityPolicyPage.ts
@@ -0,0 +1,125 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {Locator, Page, expect} from '@playwright/test';
+
+import {clickAndExpectToBeVisible} from '../../utils/clickAndExpectToBeVisible';
+import {waitForAlert} from '../../utils/waitForAlert';
+import {ApplicationsMenuPage} from '../product-navigation-applications-menu/ApplicationsMenuPage';
+
+export class ContentSecurityPolicyPage {
+	readonly actions: Locator;
+	readonly applicationsMenuPage: ApplicationsMenuPage;
+	readonly contentSecurityPolicy: Locator;
+	readonly duplicateExcludedPathsButton: Locator;
+	readonly enabled: Locator;
+	readonly newExcludedPaths: Locator;
+	readonly page: Page;
+	readonly resetDefaultValues: Locator;
+	readonly saveButton: Locator;
+	readonly updateButton: Locator;
+
+	constructor(page: Page) {
+		this.actions = page.getByRole('button', {name: 'Actions'});
+		this.applicationsMenuPage = new ApplicationsMenuPage(page);
+		this.contentSecurityPolicy = page.getByLabel('Content Security Policy');
+		this.duplicateExcludedPathsButton = page
+			.locator('.ddm-form-field-repeatable-add-button')
+			.first();
+		this.enabled = page.getByLabel('Enabled');
+		this.newExcludedPaths = page.locator('textarea:empty');
+		this.page = page;
+		this.resetDefaultValues = page.getByText('Reset Default Values');
+		this.saveButton = page.getByRole('button', {name: 'Save'});
+		this.updateButton = page.getByRole('button', {name: 'Update'});
+	}
+
+	async addExcludedPaths(excludedPaths: string) {
+		await this.duplicateExcludedPathsButton.click();
+
+		await this.newExcludedPaths.fill(excludedPaths);
+
+		await expect(
+			this.page.locator('textarea[id*="excludedPaths"]', {
+				hasText: excludedPaths,
+			})
+		).toBeVisible();
+	}
+
+	async disableCSP() {
+		await this.enabled.uncheck();
+
+		await expect(this.enabled).not.toBeChecked();
+
+		await this.saveConfiguration();
+	}
+
+	async enableCSP() {
+		await this.enabled.check();
+
+		await expect(this.enabled).toBeChecked();
+
+		await this.saveConfiguration();
+	}
+
+	async goTo() {
+		await this.applicationsMenuPage.goToInstanceSettings();
+
+		await this.page
+			.getByRole('link', {name: 'Content Security Policy'})
+			.click();
+
+		await this.enabled.waitFor();
+	}
+
+	async removeExcludedPaths(excludedPaths: string) {
+		const excludedPathsParentDiv = this.page.locator(
+			'div[data-field-reference="excludedPaths"]',
+			{
+				has: this.page.locator(`div[title="${excludedPaths}"]`),
+			}
+		);
+
+		const removeExcludedPathsButton = excludedPathsParentDiv.locator(
+			'button[title="Remove"]'
+		);
+
+		await removeExcludedPathsButton.click();
+
+		await expect(excludedPathsParentDiv).not.toBeVisible();
+	}
+
+	async resetCSPConfiguration() {
+		if (await this.actions.isVisible()) {
+			await clickAndExpectToBeVisible({
+				autoClick: true,
+				target: this.resetDefaultValues,
+				trigger: this.actions,
+			});
+
+			await waitForAlert(this.page);
+		}
+	}
+
+	async saveConfiguration() {
+		if (await this.page.isVisible('button:has-text("Update")')) {
+			await this.updateButton.click();
+		}
+		else {
+			await this.saveButton.click();
+		}
+
+		await waitForAlert(
+			this.page,
+			`Success:Your request completed successfully.`
+		);
+	}
+
+	async setPolicy(policy: string) {
+		await this.contentSecurityPolicy.fill(policy);
+
+		await expect(this.contentSecurityPolicy).toHaveText(policy);
+	}
+}
