9294558827cb579b6785086e0e074104e7dae8c8
Sandro Chinea <sandrochinea@gmail.com>
2025-02-26 15:07:30 +0000

    LPD-48502 Allow converting to pascal or camel styles

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/utils/normalizeName.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/utils/normalizeName.ts
@@ -3,15 +3,34 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export default function normalizeName(name: string) {
+type Options = {
+	style?: 'camel' | 'pascal';
+};
 
-	// Remove special characters
+export default function normalizeName(name: string, {style}: Options = {}) {
 
-	let nextName = name.replace(/[^A-Z0-9]/gi, '');
+	// Remove special characters, replace dashes with spaces and split
 
-	// Capitalize first letter
+	let splits = name
+		.replace(/[^A-Z0-9 -]/gi, '')
+		.replaceAll('-', ' ')
+		.split(' ');
 
-	nextName = nextName.charAt(0).toUpperCase() + nextName.slice(1);
+	// Convert to correct style if specified
 
-	return nextName;
+	if (style === 'pascal') {
+		splits = splits.map(
+			(split) =>
+				split.charAt(0).toUpperCase() + split.slice(1).toLowerCase()
+		);
+	}
+	else if (style === 'camel') {
+		splits = splits.map((split, index) =>
+			index === 0
+				? split.toLowerCase()
+				: split.charAt(0).toUpperCase() + split.slice(1).toLowerCase()
+		);
+	}
+
+	return splits.join('');
 }
