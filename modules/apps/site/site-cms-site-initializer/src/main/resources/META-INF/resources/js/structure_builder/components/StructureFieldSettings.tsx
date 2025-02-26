13a5b27f3935f5e421c52d4cf6e93e43a221f85c
Victor Galan <victor.galan@liferay.com>
2025-02-26 17:00:36 +0100

    LPD-48502 Add tabs to field settings

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/StructureFieldSettings.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/structure_builder/components/StructureFieldSettings.tsx
@@ -4,7 +4,9 @@
  */
 
 import ClayBreadcrumb from '@clayui/breadcrumb';
+import ClayLabel from '@clayui/label';
 import ClayLayout from '@clayui/layout';
+import ClayTabs from '@clayui/tabs';
 import React from 'react';
 
 import {useSelector, useStateDispatch} from '../contexts/StateContext';
@@ -22,8 +24,9 @@ export default function StructureFieldSettings({
 	const field = useSelector(selectStructureField(fieldName));
 
 	return (
-		<ClayLayout.ContainerFluid view>
+		<ClayLayout.ContainerFluid size="md" view>
 			<ClayBreadcrumb
+				className="mb-3"
 				items={[
 					{
 						label: structureLabel,
@@ -39,6 +42,36 @@ export default function StructureFieldSettings({
 					},
 				]}
 			/>
+
+			<ClayTabs>
+				<ClayTabs.List>
+					<ClayTabs.Item>
+						{Liferay.Language.get('general')}
+					</ClayTabs.Item>
+
+					<ClayTabs.Item>
+						{Liferay.Language.get('search')}
+					</ClayTabs.Item>
+				</ClayTabs.List>
+
+				<ClayTabs.Panels fade>
+					<ClayTabs.TabPane>
+						<GeneralTab field={field!} />
+					</ClayTabs.TabPane>
+
+					<ClayTabs.TabPane>
+						<SearchTab />
+					</ClayTabs.TabPane>
+				</ClayTabs.Panels>
+			</ClayTabs>
 		</ClayLayout.ContainerFluid>
 	);
+}
+
+function GeneralTab({field}: {field: Field}) {
+	return <div></div>;
+}
+
+function SearchTab() {
+	return <div>Search</div>;
 }
