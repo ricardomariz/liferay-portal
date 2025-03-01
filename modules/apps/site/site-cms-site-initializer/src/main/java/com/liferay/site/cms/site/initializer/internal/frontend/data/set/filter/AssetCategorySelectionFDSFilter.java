c3e4b2fc1528e1aa01e469ea7f86033a37766332
Marco Leo <marco.leo@liferay.com>
2025-03-01 14:38:54 +0100

    LPD-33236 Add Filters in the CMS FDS

---
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/filter/AssetCategorySelectionFDSFilter.java
@@ -0,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.frontend.data.set.filter;
+
+import com.liferay.frontend.data.set.constants.FDSEntityFieldTypes;
+import com.liferay.frontend.data.set.filter.BaseSelectionFDSFilter;
+import com.liferay.frontend.data.set.filter.FDSFilter;
+import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	property = {
+		"frontend.data.set.name=" + CMSSiteInitializerFDSNames.ALL_SECTION,
+		"frontend.data.set.name=" + CMSSiteInitializerFDSNames.FILES_SECTION
+	},
+	service = FDSFilter.class
+)
+public class AssetCategorySelectionFDSFilter extends BaseSelectionFDSFilter {
+
+	@Override
+	public String getAPIURL() {
+		return "/o/headless-admin-taxonomy/v1.0/taxonomy-categories/0" +
+			"/taxonomy-categories?sort=name:asc";
+	}
+
+	@Override
+	public String getEntityFieldType() {
+		return FDSEntityFieldTypes.COLLECTION;
+	}
+
+	@Override
+	public String getId() {
+		return "taxonomyCategoryIds";
+	}
+
+	@Override
+	public String getItemKey() {
+		return "id";
+	}
+
+	@Override
+	public String getItemLabel() {
+		return "name";
+	}
+
+	@Override
+	public String getLabel() {
+		return "category";
+	}
+
+	@Override
+	public boolean isAutocompleteEnabled() {
+		return true;
+	}
+
+}
---
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/filter/WorkflowStatusSelectionFDSFilter.java
@@ -0,0 +1,76 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.frontend.data.set.filter;
+
+import com.liferay.frontend.data.set.filter.BaseSelectionFDSFilter;
+import com.liferay.frontend.data.set.filter.FDSFilter;
+import com.liferay.frontend.data.set.filter.SelectionFDSFilterItem;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	property = {
+		"frontend.data.set.name=" + CMSSiteInitializerFDSNames.ALL_SECTION,
+		"frontend.data.set.name=" + CMSSiteInitializerFDSNames.CONTENTS_SECTION,
+		"frontend.data.set.name=" + CMSSiteInitializerFDSNames.FILES_SECTION
+	},
+	service = FDSFilter.class
+)
+public class WorkflowStatusSelectionFDSFilter extends BaseSelectionFDSFilter {
+
+	@Override
+	public String getId() {
+		return "status";
+	}
+
+	@Override
+	public String getLabel() {
+		return "status";
+	}
+
+	@Override
+	public List<SelectionFDSFilterItem> getSelectionFDSFilterItems(
+		Locale locale) {
+
+		return ListUtil.fromArray(
+			new SelectionFDSFilterItem(
+				WorkflowConstants.LABEL_APPROVED,
+				WorkflowConstants.STATUS_APPROVED),
+			new SelectionFDSFilterItem(
+				WorkflowConstants.LABEL_DENIED,
+				WorkflowConstants.STATUS_DENIED),
+			new SelectionFDSFilterItem(
+				WorkflowConstants.LABEL_DRAFT, WorkflowConstants.STATUS_DRAFT),
+			new SelectionFDSFilterItem(
+				WorkflowConstants.LABEL_EXPIRED,
+				WorkflowConstants.STATUS_EXPIRED),
+			new SelectionFDSFilterItem(
+				WorkflowConstants.LABEL_IN_TRASH,
+				WorkflowConstants.STATUS_IN_TRASH),
+			new SelectionFDSFilterItem(
+				WorkflowConstants.LABEL_INACTIVE,
+				WorkflowConstants.STATUS_INACTIVE),
+			new SelectionFDSFilterItem(
+				WorkflowConstants.LABEL_INCOMPLETE,
+				WorkflowConstants.STATUS_INCOMPLETE),
+			new SelectionFDSFilterItem(
+				WorkflowConstants.LABEL_PENDING,
+				WorkflowConstants.STATUS_PENDING),
+			new SelectionFDSFilterItem(
+				WorkflowConstants.LABEL_SCHEDULED,
+				WorkflowConstants.STATUS_SCHEDULED));
+	}
+
+}
