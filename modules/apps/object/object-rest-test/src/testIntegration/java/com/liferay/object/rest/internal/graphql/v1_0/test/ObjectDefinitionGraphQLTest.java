ba0ed79ec60ec44b3eb5dfa40b5ba67fc94b280c
Alberto Moreno <alberto.moreno@liferay.com>
2025-03-03 11:12:11 +0100

    LPD-49283 Make object definition not localized to prevent 'PrincipalException'

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/graphql/v1_0/test/ObjectDefinitionGraphQLTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/graphql/v1_0/test/ObjectDefinitionGraphQLTest.java
@@ -237,7 +237,7 @@ public class ObjectDefinitionGraphQLTest {
 
 		ObjectDefinition objectDefinition =
 			ObjectDefinitionTestUtil.publishObjectDefinition(
-				true, ObjectDefinitionTestUtil.getRandomName(),
+				false, ObjectDefinitionTestUtil.getRandomName(),
 				Arrays.asList(
 					new PicklistObjectFieldBuilder(
 					).userId(
