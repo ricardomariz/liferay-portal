c3e8085f6ca308db791ccf9fc5a8bcaf12e6a254
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2025-03-04 19:46:21 +0100

    LPD-48498 We need to set it to empty since the json parser is not working fine and testPatchSiteSiteByExternalReferenceCodePageExperience fail

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageExperienceResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/PageExperienceResourceTest.java
@@ -191,23 +191,7 @@ public class PageExperienceResourceTest
 
 		pageExperience.setName_i18n(
 			Collections.singletonMap("en-US", RandomTestUtil.randomString()));
-
-		PageElement firstContainerPageElement = new PageElement();
-
-		firstContainerPageElement.setPageElements(new PageElement[0]);
-		firstContainerPageElement.setPosition(0);
-		firstContainerPageElement.setType(PageElement.Type.CONTAINER);
-
-		PageElement secondContainerPageElement = new PageElement();
-
-		secondContainerPageElement.setPageElements(new PageElement[0]);
-		secondContainerPageElement.setPosition(1);
-		secondContainerPageElement.setType(PageElement.Type.CONTAINER);
-
-		pageExperience.setPageElements(
-			new PageElement[] {
-				firstContainerPageElement, secondContainerPageElement
-			});
+		pageExperience.setPageElements(new PageElement[0]);
 
 		SegmentsEntry segmentsEntry = SegmentsTestUtil.addSegmentsEntry(
 			testGroup.getGroupId());
