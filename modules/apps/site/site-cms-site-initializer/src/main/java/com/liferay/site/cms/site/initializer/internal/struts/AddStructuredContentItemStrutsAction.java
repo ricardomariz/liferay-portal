db9c31079541f0b9551884f0f709a80c9dcd9d47
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-03-05 11:35:58 +0100

    LPD-50665 Create the default display page if it does not exist adding a form container to edit the object entry

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/struts/AddStructuredContentItemStrutsAction.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/struts/AddStructuredContentItemStrutsAction.java
@@ -5,8 +5,18 @@
 
 package com.liferay.site.cms.site.initializer.internal.struts;
 
+import com.liferay.fragment.listener.FragmentEntryLinkListener;
+import com.liferay.fragment.listener.FragmentEntryLinkListenerRegistry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.layout.constants.LayoutTypeSettingsConstants;
+import com.liferay.layout.manager.FormManager;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.layout.util.structure.FormStyledLayoutStructureItem;
+import com.liferay.layout.util.structure.LayoutStructure;
 import com.liferay.object.constants.ObjectDefinitionConstants;
 import com.liferay.object.constants.ObjectEntryFolderConstants;
 import com.liferay.object.model.ObjectDefinition;
@@ -15,6 +25,7 @@ import com.liferay.object.service.ObjectDefinitionService;
 import com.liferay.object.service.ObjectEntryService;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.FriendlyURLResolver;
@@ -29,10 +40,14 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 import javax.servlet.http.HttpServletRequest;
@@ -77,8 +92,13 @@ public class AddStructuredContentItemStrutsAction implements StrutsAction {
 			_layoutPageTemplateEntryLocalService.
 				fetchDefaultLayoutPageTemplateEntry(groupId, classNameId, 0);
 
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			httpServletRequest);
+
 		if (layoutPageTemplateEntry == null) {
-			return null;
+			layoutPageTemplateEntry = _addDefaultLayoutPageTemplateEntry(
+				classNameId, groupId, objectDefinition.getName(),
+				serviceContext);
 		}
 
 		Group group = _groupLocalService.getGroup(groupId);
@@ -91,9 +111,6 @@ public class AddStructuredContentItemStrutsAction implements StrutsAction {
 
 		Layout layout = _layoutLocalService.fetchLayout(
 			layoutPageTemplateEntry.getPlid());
-
-		ServiceContext serviceContext = ServiceContextFactory.getInstance(
-			httpServletRequest);
 
 		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_SAVE_DRAFT);
 
@@ -116,6 +133,93 @@ public class AddStructuredContentItemStrutsAction implements StrutsAction {
 		return null;
 	}
 
+	private LayoutPageTemplateEntry _addDefaultLayoutPageTemplateEntry(
+			long classNameId, long groupId, String name,
+			ServiceContext serviceContext)
+		throws Exception {
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			_layoutPageTemplateEntryLocalService.addLayoutPageTemplateEntry(
+				null, serviceContext.getUserId(), groupId, 0, null, classNameId,
+				0, name, LayoutPageTemplateEntryTypeConstants.DISPLAY_PAGE, 0,
+				true, 0, 0, 0, WorkflowConstants.STATUS_APPROVED,
+				serviceContext);
+
+		Layout layout = _layoutLocalService.getLayout(
+			layoutPageTemplateEntry.getPlid());
+
+		Layout draftLayout = layout.fetchDraftLayout();
+
+		LayoutPageTemplateStructure layoutPageTemplateStructure =
+			_layoutPageTemplateStructureLocalService.
+				fetchLayoutPageTemplateStructure(
+					draftLayout.getGroupId(), draftLayout.getPlid());
+
+		if (layoutPageTemplateStructure == null) {
+			return layoutPageTemplateEntry;
+		}
+
+		long segmentsExperienceId =
+			_segmentsExperienceLocalService.fetchDefaultSegmentsExperienceId(
+				draftLayout.getPlid());
+
+		LayoutStructure layoutStructure = LayoutStructure.of(
+			layoutPageTemplateStructure.getData(segmentsExperienceId));
+
+		FormStyledLayoutStructureItem formStyledLayoutStructureItem =
+			(FormStyledLayoutStructureItem)
+				layoutStructure.addFormStyledLayoutStructureItem(
+					layoutStructure.getMainItemId(), 0);
+
+		formStyledLayoutStructureItem.setClassNameId(
+			layoutPageTemplateEntry.getClassNameId());
+
+		List<FragmentEntryLink> addedFragmentEntryLinks = new ArrayList<>();
+
+		_formManager.addFragmentEntryLinksLayoutStructureItems(
+			addedFragmentEntryLinks, _jsonFactory.createJSONObject(),
+			formStyledLayoutStructureItem, true, draftLayout, layoutStructure,
+			LocaleUtil.getMostRelevantLocale(), segmentsExperienceId,
+			serviceContext, null);
+
+		_layoutPageTemplateStructureLocalService.
+			updateLayoutPageTemplateStructureData(
+				draftLayout.getGroupId(), draftLayout.getPlid(),
+				segmentsExperienceId, layoutStructure.toString());
+
+		for (FragmentEntryLink addedFragmentEntryLink :
+				addedFragmentEntryLinks) {
+
+			for (FragmentEntryLinkListener fragmentEntryLinkListener :
+					_fragmentEntryLinkListenerRegistry.
+						getFragmentEntryLinkListeners()) {
+
+				fragmentEntryLinkListener.onAddFragmentEntryLink(
+					addedFragmentEntryLink);
+			}
+		}
+
+		_layoutLocalService.copyLayoutContent(draftLayout, layout);
+
+		draftLayout = _layoutLocalService.getLayout(draftLayout.getPlid());
+
+		draftLayout.setStatus(WorkflowConstants.STATUS_APPROVED);
+
+		draftLayout = _layoutLocalService.updateLayout(draftLayout);
+
+		UnicodeProperties typeSettingsUnicodeProperties =
+			draftLayout.getTypeSettingsProperties();
+
+		typeSettingsUnicodeProperties.put(
+			LayoutTypeSettingsConstants.KEY_PUBLISHED, Boolean.TRUE.toString());
+
+		_layoutLocalService.updateLayout(
+			layout.getGroupId(), layout.isPrivateLayout(), layout.getLayoutId(),
+			typeSettingsUnicodeProperties.toString());
+
+		return layoutPageTemplateEntry;
+	}
+
 	private String _getURLSeparator() {
 		FriendlyURLResolver friendlyURLResolver =
 			FriendlyURLResolverRegistryUtil.
@@ -132,7 +236,17 @@ public class AddStructuredContentItemStrutsAction implements StrutsAction {
 	}
 
 	@Reference
+	private FormManager _formManager;
+
+	@Reference
+	private FragmentEntryLinkListenerRegistry
+		_fragmentEntryLinkListenerRegistry;
+
+	@Reference
 	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 	@Reference
 	private LayoutLocalService _layoutLocalService;
@@ -142,6 +256,10 @@ public class AddStructuredContentItemStrutsAction implements StrutsAction {
 		_layoutPageTemplateEntryLocalService;
 
 	@Reference
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
+
+	@Reference
 	private ObjectDefinitionService _objectDefinitionService;
 
 	@Reference
@@ -149,5 +267,8 @@ public class AddStructuredContentItemStrutsAction implements StrutsAction {
 
 	@Reference
 	private Portal _portal;
+
+	@Reference
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
 
 }
