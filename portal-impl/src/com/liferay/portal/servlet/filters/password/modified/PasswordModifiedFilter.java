30487f7a3ff95122ba66cff8a076a385f592f4c6
ChrisKian <christopher.kian@liferay.com>
2025-03-05 08:27:17 -0800

    LPD-50660 SF

--- portal-impl/src/com/liferay/portal/servlet/filters/password/modified/PasswordModifiedFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/password/modified/PasswordModifiedFilter.java
@@ -53,19 +53,18 @@ public class PasswordModifiedFilter extends BasePortalFilter {
 
 		String contextPath = PortalUtil.getPathContext();
 
-		String proxyPath = PortalUtil.getPathProxy();
+		if (Validator.isNotNull(contextPath)) {
+			String proxyPath = PortalUtil.getPathProxy();
 
-		if (Validator.isNotNull(contextPath) &&
-			Validator.isNotNull(proxyPath) &&
-			contextPath.startsWith(proxyPath)) {
+			if (Validator.isNotNull(proxyPath) &&
+				contextPath.startsWith(proxyPath)) {
 
-			contextPath = contextPath.substring(proxyPath.length());
-		}
+				contextPath = contextPath.substring(proxyPath.length());
+			}
 
-		if (Validator.isNotNull(contextPath) &&
-			!contextPath.equals(StringPool.SLASH)) {
-
-			requestURI = requestURI.substring(contextPath.length());
+			if (!contextPath.equals(StringPool.SLASH)) {
+				requestURI = requestURI.substring(contextPath.length());
+			}
 		}
 
 		return HttpComponentsUtil.removePathParameters(requestURI);
