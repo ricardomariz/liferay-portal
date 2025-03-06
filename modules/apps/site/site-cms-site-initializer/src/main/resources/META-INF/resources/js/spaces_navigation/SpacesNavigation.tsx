615db2785787c708a7ebafa3998eafd17e5a9511
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2025-03-06 12:33:56 +0100

    LPD-49732 Align panel title

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/spaces_navigation/SpacesNavigation.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/spaces_navigation/SpacesNavigation.tsx
@@ -32,7 +32,7 @@ const SpacesNavigation: React.FC = () => {
 		<ClayPanel
 			collapsable
 			displayTitle={
-				<ClayPanel.Title>
+				<ClayPanel.Title className="align-items-center d-flex font-weight-semi-bold justify-content-between text-2 text-uppercase">
 					<span>{Liferay.Language.get('spaces')}</span>
 
 					<span className="float-right mr-2">
