7d3c02db03f1b646592d6d9f076c02699e4f41c9
Luis Ortiz <luis.ortiz@liferay.com>
2025-05-19 15:52:17 +0200

    LPD-52638 Moving upgrade tests to internal package as expected

--- modules/apps/static/portal-configuration/portal-configuration-persistence-test/src/testIntegration/java/com/liferay/portal/configuration/persistence/internal/upgrade/test/UpgradeConfigurationPidUpgradeProcessTest.java
+++ modules/apps/static/portal-configuration/portal-configuration-persistence-test/src/testIntegration/java/com/liferay/portal/configuration/persistence/internal/upgrade/test/UpgradeConfigurationPidUpgradeProcessTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.portal.configuration.persistence.upgrade.test;
+package com.liferay.portal.configuration.persistence.internal.upgrade.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.string.CharPool;
