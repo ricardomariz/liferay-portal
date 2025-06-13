7b78bd72bbc553235f0f76cc0e13e6331d531970
Jorge Avalos <jorge.avalos@liferay.com>
2025-06-12 22:32:34 -0700

    LPD-56186 condense and hardcode tablename

--- portal-impl/src/com/liferay/portal/verify/PreupgradeVerifyDatabasePrivileges.java
+++ portal-impl/src/com/liferay/portal/verify/PreupgradeVerifyDatabasePrivileges.java
@@ -5,7 +5,6 @@
 
 package com.liferay.portal.verify;
 
-import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBInspector;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
@@ -20,58 +19,48 @@ public class PreupgradeVerifyDatabasePrivileges
 
 	@Override
 	protected void doVerify() throws Exception {
-		String tempTableName = "temp_permission_check";
-
 		DB db = DBManagerUtil.getDB();
 
 		DBInspector dbInspector = new DBInspector(connection);
 
 		try {
-			if (dbInspector.hasTable(tempTableName)) {
-				db.runSQL("drop table " + tempTableName);
+			if (dbInspector.hasTable("temp_permission_check")) {
+				db.runSQL("drop table temp_permission_check");
 			}
 
 			db.runSQL(
-				StringBundler.concat(
-					"create table ", tempTableName,
-					"(column1 LONG not null primary key, ",
-					"column2 VARCHAR(75))"));
+				"create table temp_permission_check (column1 int not null)");
 
 			db.updateIndexes(
-				connection, tempTableName,
-				StringBundler.concat(
-					"create index ix_temp on ", tempTableName, " (column2)"),
+				connection, "temp_permission_check",
+				"create index ix_temp on temp_permission_check (column1)",
 				true);
 
-			alterTableAddColumn(tempTableName, "column3", "long");
+			alterTableAddColumn("temp_permission_check", "column2", "int");
 
 			db.runSQL(
-				StringBundler.concat(
-					"insert into ", tempTableName,
-					" (column1, column2, column3) values (1,1,1)"));
+				"insert into temp_permission_check(column1, column2) values " +
+					"(1,1)");
 
 			db.runSQL(
-				StringBundler.concat(
-					"update ", tempTableName,
-					" set column2 = 2 where column1 = 1"));
+				"update temp_permission_check set column2 = 2 where column1 " +
+					"= 1");
 
 			PreparedStatement preparedStatement = connection.prepareStatement(
-				StringBundler.concat(
-					"select 1 from ", tempTableName, " where column1 = 1"));
+				"select 1 from temp_permission_check where column1 = 1");
 
 			preparedStatement.executeQuery();
 
-			db.runSQL(
-				StringBundler.concat(
-					"delete from ", tempTableName, " where column1 = 1"));
+			db.runSQL("delete from temp_permission_check where column1 = 1");
 
-			if (dbInspector.hasTable(tempTableName)) {
-				db.runSQL("drop table " + tempTableName);
+			if (dbInspector.hasTable("tempTableName")) {
+				db.runSQL("drop table temp_permission_check");
 			}
 		}
 		catch (Exception exception) {
 			throw new VerifyException(
-				"Database user is missing privileges", exception);
+				"Database user is missing privileges: " +
+					exception.getMessage());
 		}
 	}
 
