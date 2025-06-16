ef2394e01bf13c9825b0a7f93b8bafa5f77fe3a2
Brian Chan <brian.chan@liferay.com>
2025-06-16 20:51:57 -0300

    LPD-57621 Just delcared ERC and finished using it

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/util/PageSpecificationsTestUtil.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/util/PageSpecificationsTestUtil.java
@@ -215,6 +215,7 @@ public class PageSpecificationsTestUtil {
 			Assert.assertEquals(
 				externalReferenceCode,
 				pageSpecification.getExternalReferenceCode());
+
 			Assert.assertEquals(
 				PageSpecification.Status.APPROVED,
 				pageSpecification.getStatus());
