cca041f69b972474440d7fa2070a0521581e19f0
José Abelenda <jose.abelenda@liferay.com>
2025-06-17 10:22:09 -0300

    LRSD-9189 trim

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-spring-boot/src/main/java/com/liferay/learn/LearnRestController.java
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-spring-boot/src/main/java/com/liferay/learn/LearnRestController.java
@@ -449,7 +449,7 @@ public class LearnRestController extends BaseRestController {
 			"^<speak>", ""
 		).replaceFirst(
 			"</speak>$", ""
-		);
+		).trim();
 		StringBuilder sb = new StringBuilder();
 		List<String> parts = new ArrayList<>();
 
